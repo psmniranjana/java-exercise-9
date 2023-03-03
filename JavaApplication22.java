12e4a50f0ff79248f025e4b392dfd7ed4a8a7ae1
psmniranjana <117069472+psmniranjana@users.noreply.github.com>
2023-03-03 19:36:23 +0530

    Add files via upload

---
+++ JavaApplication22.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication22;
+ import java.util.Scanner;
+
+public class JavaApplication22 {
+
+    
+    public static void main(String[] args) {
+      
+
+
+    
+
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble(); 
+        convert(num);
+    }
+     public static void convert(double num){
+         System.out.println(num*12);
+     }
+}
+        
+    }
+    
+}
